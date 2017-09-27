d8614d324bb5f0c83917d328c3b4b14769d110b6
Samuel Martins <sfelix_martins@hotmail.com>
2017-09-27 22:21:09 +0000

    Enabled cors to passport routes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use Laravel\Passport\Passport;
 use Modules\User\Entities\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -28,11 +29,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Passport::routes();
-
-        // Passport::tokensExpireIn(Carbon::now()->addMinutes(1));
-
-        // Passport::refreshTokensExpireIn(Carbon::now()->addMinutes(2));
+        Route::group(['prefix' => 'v1', 'middleware' => 'api'], function () {
+            Passport::routes();
+        });
 
         Gate::define('users.view', function (User $user, $id) {
             return $user->id == $id;
